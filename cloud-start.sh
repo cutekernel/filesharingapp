af407fc958fb712409feeabe23b6ec1f3757a088
vuvu <cutekernel@gmail.com>
2023-05-15 18:47:41 -0400

    added cloud script

--- cloud-start.sh
+++ cloud-start.sh
@@ -3,5 +3,5 @@ docker-compose down
 docker rmi $(docker images -q)
 docker rmi $(docker images -f "dangling=true" -q) -f
 docker-compose up -d --build
-docker exec -i mariadb mysql -uroot -ppassword filesharingdb < /appdata/data.sql
+docker exec -it mariadb mysql -uroot -ppassword filesharingdb < /appdata/data.sql
 docker-compose logs -f
